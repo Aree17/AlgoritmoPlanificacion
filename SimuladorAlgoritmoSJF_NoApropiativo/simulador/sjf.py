25df7f237ce87c1a512321992c4c954f6895c8b9
Michu117 <fabricio.ruiz@unl.edu.ec>
2026-01-09 23:26:33 -0500

    Lógica SJF

--- SimuladorAlgoritmoSJF_NoApropiativo/simulador/sjf.py
+++ SimuladorAlgoritmoSJF_NoApropiativo/simulador/sjf.py
@@ -1,66 +1,116 @@
-def sjf_no_apropiativo(procesos):
+def sjf_simular(procesos):
+
     tiempo = 0
-    listos = []
-    cola_es = []
     gantt = []
 
-    # inicializar campos internos
+    pendientes = procesos.copy()
+    listos = []
+    en_es = []
+    completados = []
+
+    estado = {}
+
     for p in procesos:
-        p.cpu_restante = p.rafaga_cpu
-        p.estado = "nuevo"
-        p.fin_es = None
+        estado[p.nombre] = {
+            "llegada": p.tiempo_llegada,
+            "rafaga": p.rafaga_cpu,
+            "es": p.es if isinstance(p.es, list) else [],
+            "es_index": 0,
+            "ejecutado": 0,
+            "fin": None
+        }
 
-    procesos = sorted(procesos, key=lambda p: p.tiempo_llegada)
+    cpl = []
+    ces = []
 
-    while procesos or listos or cola_es:
+    while pendientes or listos or en_es:
 
-        # Llegadas
-        while procesos and procesos[0].tiempo_llegada <= tiempo:
-            p = procesos.pop(0)
-            listos.append(p)
-
-        # Retorno de E/S
-        for p in cola_es[:]:
-            if p.fin_es <= tiempo:
-                cola_es.remove(p)
+        # llegadas
+        for p in pendientes[:]:
+            if p.tiempo_llegada <= tiempo:
                 listos.append(p)
+                pendientes.remove(p)
+
+        # retorno E/S
+        for e in en_es[:]:
+            if e["fin"] <= tiempo:
+                listos.append(e["proceso"])
+                en_es.remove(e)
 
         if listos:
-            listos.sort(key=lambda p: p.cpu_restante)
-            p = listos.pop(0)
 
+            listos.sort(key=lambda x: (
+                estado[x.nombre]["rafaga"] - estado[x.nombre]["ejecutado"],
+                estado[x.nombre]["llegada"]
+            ))
+
+            p = listos.pop(0)
+            cpl.append(p.nombre)
+
+            info = estado[p.nombre]
+            restante = info["rafaga"] - info["ejecutado"]
             inicio = tiempo
 
-            # ¿tiene E/S pendiente?
-            ejecutado = p.rafaga_cpu - p.cpu_restante
+            # próxima E/S
+            if info["es_index"] < len(info["es"]) and len(info["es"][info["es_index"]]) == 2:
+                proxima_es = info["es"][info["es_index"]][0]
+                dur_es = info["es"][info["es_index"]][1]
+            else:
+                proxima_es = None
+                dur_es = None
 
-            if p.inicio_es is not None and ejecutado < p.inicio_es:
-                hasta_es = p.inicio_es - ejecutado
+            if proxima_es is not None and info["ejecutado"] < proxima_es:
+                ejecutar = min(restante, proxima_es - info["ejecutado"])
+            else:
+                ejecutar = restante
 
-                if hasta_es < p.cpu_restante:
-                    # Ejecuta hasta E/S
-                    tiempo += hasta_es
-                    p.cpu_restante -= hasta_es
+            fin = tiempo + ejecutar
+            gantt.append((p.nombre, inicio, fin))
 
-                    gantt.append((p.nombre, inicio, tiempo))
+            info["ejecutado"] += ejecutar
+            tiempo = fin
 
-                    # entra a E/S
-                    p.fin_es = tiempo + p.duracion_es
-                    cola_es.append(p)
-                    continue
+            # entra a E/S
+            if proxima_es is not None and info["ejecutado"] == proxima_es:
+                info["es_index"] += 1
+                en_es.append({
+                    "proceso": p,
+                    "fin": tiempo + dur_es
+                })
+                ces.append(p.nombre)
 
-            # Ejecuta lo restante
-            tiempo += p.cpu_restante
-            gantt.append((p.nombre, inicio, tiempo))
-            p.cpu_restante = 0
+            elif info["ejecutado"] == info["rafaga"]:
+                info["fin"] = tiempo
+                completados.append(p)
+
+            else:
+                listos.append(p)
 
         else:
             tiempo += 1
 
-    return gantt
+    # métricas
+    resultados = []
+    total_tep = 0
+    total_teje = 0
 
-def metricas(completados):
-    for c in completados:
-        c["TEjeP"] = c["fin"] - c["llegada"]
-        c["TEP"] = c["TEjeP"] - c["rafaga"]
-    return completados
+    for p in completados:
+        info = estado[p.nombre]
+        total_es = sum(d[1] for d in info["es"] if len(d) == 2)
+
+        tep = info["fin"] - info["llegada"] - info["rafaga"] - total_es
+        teje = info["fin"] - info["llegada"]
+
+        total_tep += tep
+        total_teje += teje
+
+        resultados.append({
+            "proceso": p.nombre,
+            "TEP": tep,
+            "TEjeP": teje
+        })
+
+    prom_tep = total_tep / len(resultados) if resultados else 0
+    prom_teje = total_teje / len(resultados) if resultados else 0
+
+    return gantt, cpl, ces, resultados, prom_tep, prom_teje
--- SimuladorAlgoritmoSJF_NoApropiativo/simulador/views.py
+++ SimuladorAlgoritmoSJF_NoApropiativo/simulador/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ProcesoForm
 from .models import Proceso
+from .sjf import sjf_simular
+
 
 def ingresar_proceso(request):
     if request.method == 'POST':
@@ -29,117 +31,7 @@ def eliminar_todos_procesos(request):
 def simular(request):
     procesos = list(Proceso.objects.all())
 
-    tiempo = 0
-    gantt = []
-
-    pendientes = procesos.copy()
-    listos = []
-    en_es = []
-    completados = []
-
-    estado = {}
-
-    for p in procesos:
-        estado[p.nombre] = {
-            "llegada": p.tiempo_llegada,
-            "rafaga": p.rafaga_cpu,
-            "es": p.es if isinstance(p.es, list) else [],
-            "es_index": 0,
-            "ejecutado": 0,
-            "fin": None
-        }
-
-    cpl = []
-    ces = []
-
-    while pendientes or listos or en_es:
-
-        for p in pendientes[:]:
-            if p.tiempo_llegada <= tiempo:
-                listos.append(p)
-                pendientes.remove(p)
-
-        for e in en_es[:]:
-            if e["fin"] <= tiempo:
-                listos.append(e["proceso"])
-                en_es.remove(e)
-
-        if listos:
-
-            listos.sort(key=lambda x: (
-                estado[x.nombre]["rafaga"] - estado[x.nombre]["ejecutado"],
-                estado[x.nombre]["llegada"]
-            ))
-
-            p = listos.pop(0)
-            cpl.append(p.nombre)
-
-            info = estado[p.nombre]
-
-            restante = info["rafaga"] - info["ejecutado"]
-            inicio = tiempo
-
-            if info["es_index"] < len(info["es"]) and len(info["es"][info["es_index"]]) == 2:
-                proxima_es = info["es"][info["es_index"]][0]
-                dur_es = info["es"][info["es_index"]][1]
-            else:
-                proxima_es = None
-                dur_es = None
-
-            if proxima_es is not None and info["ejecutado"] < proxima_es:
-                ejecutar = min(restante, proxima_es - info["ejecutado"])
-            else:
-                ejecutar = restante
-
-            fin = tiempo + ejecutar
-            gantt.append((p.nombre, inicio, fin))
-
-            info["ejecutado"] += ejecutar
-            tiempo = fin
-
-            # Entra a E/S
-            if proxima_es is not None and info["ejecutado"] == proxima_es:
-                info["es_index"] += 1
-
-                en_es.append({
-                    "proceso": p,
-                    "fin": tiempo + dur_es
-                })
-
-                ces.append(p.nombre)
-
-            elif info["ejecutado"] == info["rafaga"]:
-                info["fin"] = tiempo
-                completados.append(p)
-
-            else:
-                listos.append(p)
-
-        else:
-            tiempo += 1
-
-    resultados = []
-    total_tep = 0
-    total_teje = 0
-
-    for p in completados:
-        info = estado[p.nombre]
-        total_es = sum(d[1] for d in info["es"] if len(d) == 2)
-
-        tep = info["fin"] - info["llegada"] - info["rafaga"] - total_es
-        teje = info["fin"] - info["llegada"]
-
-        total_tep += tep
-        total_teje += teje
-
-        resultados.append({
-            "proceso": p.nombre,
-            "TEP": tep,
-            "TEjeP": teje
-        })
-
-    prom_tep = total_tep / len(resultados) if resultados else 0
-    prom_teje = total_teje / len(resultados) if resultados else 0
+    gantt, cpl, ces, resultados, prom_tep, prom_teje = sjf_simular(procesos)
 
     return render(request, "resultado.html", {
         "gantt": gantt,
